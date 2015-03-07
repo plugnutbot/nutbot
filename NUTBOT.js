251e488c10b37c0dfc40ded15f7c3b8837db6f89
plugnutbot <robert-whittaker@live.co.uk>
2015-03-07 16:24:33 +0000

    Update NUTBOT.js
    line 244

--- NUTBOT.js
+++ NUTBOT.js
@@ -241,7 +241,8 @@
             commandLiteral: "!",
             blacklists: {
                 NSFW: "https://cdn.rawgit.com/plugnutbot/nutbot/0.5.8/blacklist.json",
-                OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
+                OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json
+                Coms: "https://github.com/plugnutbot/nutbot/blob/0.5.8/blacklist.json"
             }
         },
         room: {
