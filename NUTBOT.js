9a977b4bd6a885d58bc51898b723617f6e2a3fe4
plugnutbot <robert-whittaker@live.co.uk>
2015-03-07 15:38:25 +0000

    Update NUTBOT.js

--- NUTBOT.js
+++ NUTBOT.js
@@ -1467,7 +1467,7 @@
             },
 
             backgroundCommand: {
-                command: 'ba',
+                command: 'bg',
                 rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
@@ -1479,18 +1479,6 @@
                 }
             },
 
-            baCommand: {
-                command: 'ba',
-                rank: 'user',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat(basicBot.chat.brandambassador);
-                    }
-                }
-            },
 
             banCommand: {
                 command: 'ban',
